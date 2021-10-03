189ec2ca1baeb779770dd2d87e537b917ba26814
Hencccc <90599688+Hencccc@users.noreply.github.com>
2021-10-03 14:42:34 +0200

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Routing } from './routes/Routing'
+import { NavigationBar } from './components/navigationbar/NavigationBar'
+import { UserProvider } from "./shared/global/provider/UserProvider"
+import './shared/global/Global.css'
+
+
+function App() {
+  return (
+    <UserProvider>
+      <Routing>
+        <NavigationBar />
+      </Routing>
+    </UserProvider>
+  );
+}
+
+export default App;
